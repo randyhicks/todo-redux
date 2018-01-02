0a644c17b2b2f7b650a48d0237e40824aad7f494
randyh <randyh@qualtrics.com>
2018-01-01 23:40:00 -0700

    Added UI changes for list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { doToggleTodo } from '../actions';
 import { getTodo } from '../reducers/reducers';
+import { Button } from 'antd';
 
 const TodoItem = ({ todo, onToggleTodo }) => {
   const { name, id, completed } = todo;
   return (
     <div>
-      {name}
-      <button type="button" onClick={() => onToggleTodo(id)}>
+      <Button
+        size={'small'}
+        primary
+        style={{ marginRight: 15 }}
+        type={completed ? 'button' : 'primary'}
+        onClick={() => onToggleTodo(id)}
+      >
         {completed ? 'Complete' : 'Incomplete'}
-      </button>
+      </Button>
+      {name}
     </div>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,24 @@ import React from 'react';
 import TodoItem from './TodoItem';
 import { connect } from 'react-redux';
 import { getTodosAsIds } from '../reducers/reducers';
+import { List, Layout } from 'antd';
+const { Content } = Layout;
 
 const TodoList = ({ todosAsIds }) => {
   return (
-    <div>
-      {todosAsIds.map(todoId => <TodoItem key={todoId} todoId={todoId} />)}
-    </div>
+    <Layout>
+      <Content style={{ backgroundColor: '#fff', margin: 50, padding: 25 }}>
+        <List
+          size={'small'}
+          dataSource={todosAsIds}
+          renderItem={todoId => (
+            <List.Item>
+              <TodoItem key={todoId} todoId={todoId} />
+            </List.Item>
+          )}
+        />
+      </Content>
+    </Layout>
   );
 };
 
